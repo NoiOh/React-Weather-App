9193b27f59be1d1dece955c8eda5991141924c77
Noi Houangvan <45153151+NoiOh@users.noreply.github.com>
2023-02-24 18:13:38 -0500

    implemented axios

--- src/Weather.js
+++ src/Weather.js
@@ -1,6 +1,17 @@
 import React from "react";
 import axios from "axios";
 
-export default function Weather() {
-  return <h2>Weather container</h2>;
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.name} is ${Math.round(
+        response.data.main.temp
+      )}°F`
+    );
+  }
+  let key = "c819171fe0abdc14039af4ef5dda283b";
+  let units = "imperial";
+  let url = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${key}&units=${units}`;
+  axios.get(url).then(handleResponse);
+  return <h2>Weather App</h2>;
 }
