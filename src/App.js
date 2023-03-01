434a5ce223292e5c177562c1919d6d34394dabc7
Noi Houangvan <45153151+NoiOh@users.noreply.github.com>
2023-03-01 16:52:30 -0500

    moved container to different file

--- src/App.js
+++ src/App.js
@@ -4,18 +4,21 @@ import Search from "./Search";
 function App() {
   return (
     <div className="App">
-      <h1>Weather Search Engine</h1>
-      <Search />
-      <footer>
-        <a
-          href="https://github.com/NoiOh/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Open-source code
-        </a>{" "}
-        by Noi Houangvan
-      </footer>
+      <div className="container">
+        <h1>Weather Search Engine</h1>
+        <Search />
+
+        <footer>
+          <a
+            href="https://github.com/NoiOh/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-source code
+          </a>{" "}
+          by Noi Houangvan
+        </footer>
+      </div>
     </div>
   );
 }
